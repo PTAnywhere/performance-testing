2ca18831aac1bda719a7aa1bb5a608ed60c1ef95
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-09-30 17:55:46 +0100

    Explaining what the class does.

--- jPTChecker/PTChecker.java
+++ jPTChecker/PTChecker.java
@@ -14,6 +14,16 @@ import com.cisco.pt.ptmp.PacketTracerSessionFactory;
 import com.cisco.pt.ptmp.impl.PacketTracerSessionFactoryImpl;
 
 
+/**
+ * This class allows you to check when a Packet Tracer session is up and
+ * able to answer IPC requests.
+ *
+ * Also, it can be used to measure the average response time of the instance
+ * for a predefine request.
+ *
+ * Note that this class depends on the ptipc library.
+ * I cannot provide a version of it as its intellectual property belongs to Cisco.
+ */
 public class PTChecker extends PacketTracerClient {
 
     public PTChecker(String host, int port) {
