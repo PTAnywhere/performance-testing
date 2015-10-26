45228334bd226fce8f36d3f8cb9a4dad58734ae9
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-10-26 17:39:40 +0000

    Fixing methods where values to override config files can be provided.

--- ptdockertest/config.py
+++ ptdockertest/config.py
@@ -15,16 +15,19 @@ class ConfigFileReader(object):
     def set_file_path(self, file_path):
         self.config.read(file_path)
 
-    def get_log(self):
+    def get_log(self, overriden_log=None):
+        if overriden_log: return overriden_log
         return self.config.get('benchmark', 'log')
 
-    def get_db(self):
+    def get_db(self, overriden_db=None):
+        if overriden_db: return overriden_db
         return self.config.get('benchmark', 'db')
 
     def get_exposed_port(self):
         return self.config.get('benchmark', 'exposed_pt_port')
 
-    def get_docker_url(self):
+    def get_docker_url(self, overriden_url=None):
+        if overriden_url: return overriden_url
         return self.config.get('docker', 'url')
 
     def get_jar_path(self):
